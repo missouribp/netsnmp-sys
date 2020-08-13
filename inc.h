b99b8ff5579b63ddd0354d81d2b2d3a71e46717d
Brian <missouribp@gmail.com>
2020-08-12 21:43:06 -0500

    Initial attempt at snmpV5.8 compatibility

--- inc.h
+++ inc.h
@@ -1,14 +1,17 @@
 #include <net-snmp/net-snmp-config.h>
 
-#include <net-snmp/session_api.h>
-#include <net-snmp/config_api.h>
+#include <net-snmp/types.h>
 #include <net-snmp/library/snmp_api.h>
-#include <net-snmp/mib_api.h>
-#include <net-snmp/output_api.h>
-#include <net-snmp/pdu_api.h>
+#include <net-snmp/library/asn1.h>
+#include <net-snmp/library/snmp_impl.h>
+#include <net-snmp/library/snmp.h>
+#include <net-snmp/library/snmp-tc.h>
+#include <net-snmp/library/getopt.h>
+#include <net-snmp/utilities.h>
 #include <net-snmp/session_api.h>
-#include <net-snmp/snmpv3_api.h>
+#include <net-snmp/pdu_api.h>
+#include <net-snmp/mib_api.h>
 #include <net-snmp/varbind_api.h>
-
-#include <net-snmp/library/vacm.h>
-#include <net-snmp/library/container.h>
+#include <net-snmp/config_api.h>
+#include <net-snmp/output_api.h>
+#include <net-snmp/snmpv3_api.h>
